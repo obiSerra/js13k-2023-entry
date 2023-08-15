111dde04568dd20a2503417914c4a05ec5c242b6
Roberto Serra <roberto.serra@etiqa.it>
2023-08-15 10:22:39 +0200

    inview optimization

--- src/scenes/testScene.ts
+++ src/scenes/testScene.ts
@@ -5,13 +5,14 @@ import {
   PositionComponent,
   StaticPositionComponent,
 } from "../lib/components";
-import { IRenderComponent, IVec, Sprite } from "../lib/contracts";
+import { IEntity, IRenderComponent, IVec, Sprite } from "../lib/contracts";
 import { ComponentBaseEntity } from "../lib/entities";
 import { GameState, Scene } from "../lib/gameState";
 import { Player } from "../player";
 
 class Ground extends ComponentBaseEntity {
-  constructor(gs: GameState, pos: IVec) {
+  name: string;
+  constructor(gs: GameState, pos: IVec, name: string) {
     const { stage } = gs;
     super(stage, []);
     const position = new StaticPositionComponent(pos);
@@ -22,8 +23,30 @@ class Ground extends ComponentBaseEntity {
     this.addComponent(position);
     this.addComponent(renderer);
     this.addComponent(box);
+    this.name = name;
+  }
+
+  render(t: number, c: IVec) {
+    super.render(t, c);
+    const [x, y] = this.getComponent<PositionComponent>("position").p;
+    const ctx = this.stage.ctx;
+    ctx.beginPath();
+    ctx.font = "20px serif";
+    ctx.fillStyle = "white";
+    ctx.fillText(this.name, x + c[0], y + c[1] + 20);
+    ctx.closePath();
   }
 }
+
+const isInView = (e: IEntity, c: IVec, canvas: HTMLCanvasElement) => {
+  const [cx, cy] = c;
+  const p = e.getComponent<PositionComponent>("position").p;
+  const iP = [p[0] + cx, p[1] + cy];
+  const border = 100;
+  const { width, height } = canvas;
+  if (iP[0] < -border || iP[0] > width + border || iP[1] < -border || iP[1] > height + border) return false;
+  return true;
+};
 
 export const testScene = onEnd => {
   return new Scene((gs: GameState, scene: Scene) => {
@@ -47,23 +70,41 @@ export const testScene = onEnd => {
 
     scene.addEntity(player);
 
-    for (let i = 4; i < 20; i++) {
-      const ground = new Ground(gs, [i * 32, 400]);
-      scene.addEntity(ground);
+    // Generate map
+    let cnt = 0;
+    let v = 400;
+    for (let i = 3; i < 200; i++) {
+      if (i > 20) {
+        if (i % 11 === 0) continue;
+        if (i % 11 === 1) continue;
+        if (i % 11 === 2 && Math.random() < 0.5) continue;
+        if (i % 11 === 3 && Math.random() < 0.5) continue;
+      }
+      if (i % 50 == 0) v -= 64;
+
+      if (i % 15 === 0) {
+        scene.addEntity(new Ground(gs, [i * 32, v - 32], cnt.toString()));
+        cnt++;
+        if (Math.random() < 0.5) {
+          scene.addEntity(new Ground(gs, [i * 32, v - 64], cnt.toString()));
+          cnt++;
+        }
+      }
+      scene.addEntity(new Ground(gs, [i * 32, v], cnt.toString()));
+      cnt++;
+      // lastBlock = 0;
     }
 
-    scene.addEntity(new Ground(gs, [500, 368]));
-    scene.addEntity(new Ground(gs, [500, 336]));
-
     gl.onUpdate(delta => {
-      scene
-        .getEntities()
-        .filter(e => typeof e.update === "function")
+      const [x, y] = player.getComponent<PositionComponent>("position").p;
+      const cx = gs.stage.canvas.width / 2 - x;
+      const cy = gs.stage.canvas.height / 2 - y;
 
-        .forEach(e => e.update(delta));
+      const inView = scene.getEntities().filter(e => isInView(e, [cx, cy], gs.stage.canvas));
 
-      const canCollide = scene.getEntities().filter(e => !!e.components["collider"]);
-      resolveCollisions(canCollide);
+      inView.filter(e => typeof e.update === "function").forEach(e => e.update(delta));
+
+      resolveCollisions(inView.filter(e => !!e.components["collider"]));
     });
 
     gl.onRender(t => {
@@ -72,8 +113,11 @@ export const testScene = onEnd => {
       const cy = gs.stage.canvas.height / 2 - y;
 
       // scene.cameraPos = [cx, cy];
+      let toRender = scene.getEntities().filter(e => {
+        if (!e.components["render"]) return false;
+        return isInView(e, [cx, cy], gs.stage.canvas);
+      });
 
-      const toRender = scene.getEntities().filter(e => !!e.components["render"]);
       toRender.sort(
         (a, b) =>
           (b.components["render"] as IRenderComponent).renderPriority -
